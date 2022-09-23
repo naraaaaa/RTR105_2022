ef7eaf309968c9667888bdced118f23fd86bd1bb
naraaaaa <naraaaofficial@gmail.com>
2022-09-23 11:32:02 +0300

    class_05

---
+++ linux_special_variables.test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#https://www.tutorialspoint.com/unix/unix-special-variables.htm#
+echo "linux_special_variables_test : $0"
+echo "First : $1"
+echo "Second : $2"
+echo "Values : $@"
+echo "Values : $*"
+echo "Total Number of Parameters : $#"
+
+#echo "please enter text: "
+#read var_string
+#echo "you have entered: $var_string"
+
+
+
