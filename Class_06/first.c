d779ab277e7cfd6046afefdb0581ab445ff80736
naraaaaa <naraaaofficial@gmail.com>
2022-09-30 11:32:26 +0300

    class_06

---
+++ Class_06/first.c
@@ -0,0 +1,18 @@
+// komentara viena rinda - // tieka apzimets tikai komentara sakums
+//komentara cita rinda
+
+/*
+ komentara poragrafs
+izmanto  lai  komentetu kodu fragmentu
+*/
+
+int main()
+{
+//if()
+//{
+//
+//}
+return 0;
+//return 100;
+}
+
